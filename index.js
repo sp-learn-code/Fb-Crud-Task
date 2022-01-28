7ef08a79c27698bdc47d578e6f0a95deb295feb3
Jorge Zambrano <joraazam@espol.edu.ec>
2022-01-27 22:40:28 -0500

    Conexion basica de Firebase

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhYoopwUV-Ij3Zc6PjT8f_D0F0ykLWOKs",
+  authDomain: "fir-js-crud-7cee8.firebaseapp.com",
+  projectId: "fir-js-crud-7cee8",
+  storageBucket: "fir-js-crud-7cee8.appspot.com",
+  messagingSenderId: "993182314666",
+  appId: "1:993182314666:web:cca561006e163d01c320d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
